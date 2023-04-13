4db40fe399929c668126cf006426f7dbb5fd6cb2
PascualFort <fortpascual@gmail.com>
2023-04-13 03:15:31 +0200

    Isolated terminal access folder and created shell user

--- app/classes/BashProcess.py
+++ app/classes/BashProcess.py
@@ -1,4 +1,3 @@
-"""Wrapper around subprocess to run commands."""
 import subprocess
 from typing import List, Union
 
@@ -11,26 +10,25 @@ class BashProcess:
         self.strip_newlines = strip_newlines
         self.return_err_output = return_err_output
         self.location = location
-        # subprocess.run(f"cd {location}", shell=True)
 
     def run(self, commands: Union[str, List[str]]) -> str:
         """Run commands and return final output."""
         if isinstance(commands, str):
             commands = [commands]
         commands = ";".join(commands)
+
         try:
             output = subprocess.run(
-                commands,
+                "cd dst && "+commands,
                 shell=True,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
-                executable='c:/Windows/System32/WindowsPowerShell/v1.0/PowerShell.exe',
             ).stdout
             if (isinstance(output, int)):
                 output = output.decode()
         except subprocess.CalledProcessError as error:
             if self.return_err_output:
-                return error.stdout
+                return error.stdout.decode()
             return str(error)
         if self.strip_newlines:
             output = output.strip()
